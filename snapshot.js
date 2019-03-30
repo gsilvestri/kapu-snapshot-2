b11f2273a252e760d5daf1ad22415fe70ed15163
Giovanni Silvestri <gsit80@gmail.com>
2019-03-30 23:09:19 +0100

    Logger: updated format

--- snapshot.js
+++ snapshot.js
@@ -1,29 +1,52 @@
 const execSync = require('child_process').execSync;
 const constants = require('./constants');
 const readdirp = require('readdirp');
-const winston = require('winston');
+const {
+    createLogger,
+    format,
+    transports
+} = require('winston')
+const {
+    colorize,
+    combine,
+    timestamp,
+    printf
+} = format
 const findRemoveSync = require('find-remove');
 
-const logger = winston.createLogger({
+// Define your custom format with printf.
+const loggerFormat = printf(info => {
+    return `[${info.timestamp}] - ${info.level}: ${info.message}`
+})
+
+const logger = createLogger({
     level: 'info',
-    format: winston.format.simple(),
+    format: combine(
+        timestamp(),
+        loggerFormat
+    ),
     transports: [
-      //
-      // - Write to all logs with level `info` and below to `combined.log` 
-      // - Write all logs error (and below) to `error.log`.
-      //
-      new winston.transports.File({ filename: 'error.log', level: 'error' }),
-      new winston.transports.File({ filename: 'combined.log' })
+        new transports.File({
+            filename: 'snapshot_creation_error.log',
+            level: 'error'
+        }),
+        new transports.File({
+            filename: 'snapshot_creation_combined.log'
+        })
     ]
-  });
+});
 
 //
 // If we're not in production then log to the `console` with the format:
 // `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
 // 
 if (process.env.NODE_ENV !== 'production') {
-    logger.add(new winston.transports.Console({
-      format: winston.format.simple()
+    logger.add(new transports.Console({
+        format: combine(
+            timestamp(),
+            colorize(),
+            loggerFormat
+        ),
     }));
 }
 var snapshotCommand = 'yarn create:xpx:mainnet';
@@ -32,64 +55,76 @@ var snapshotCommandOptions = {
     stdio: 'inherit'
 };
 
-logger.info('Removing snapshots older than ' + constants.DELETE_OLDER_THAN_SECONDS + ' seconds');
-findRemoveSync(constants.XPX_SNAPSHOT_SOURCE + '/' + constants.XPX_NETWORK + '/', {dir: '*', file: '*.*', age: {seconds: constants.DELETE_OLDER_THAN_SECONDS}});
-logger.info(constants.XPX_SNAPSHOT_DESTINATION);
-var deleteCommand = 'find ' + constants.XPX_SNAPSHOT_DESTINATION + ' -name \"*\" -type f -mmin +' + constants.DELETE_OLDER_THAN_SECONDS/60 + ' -delete';
-var deleteCommandOptions = {
-    cwd: constants.XPX_SNAPSHOT_DESTINATION,
-    stdio: 'inherit'
-};
-execSync(deleteCommand, deleteCommandOptions);
+function createSnapshot() {
 
-logger.info('Start snapshot creation');
-execSync(snapshotCommand, snapshotCommandOptions);
-logger.info('Zip snapshot and copy to destination folder');
-
-var settings = {
-    root: constants.XPX_SNAPSHOT_SOURCE + '/' + constants.XPX_NETWORK + '/',
-    depth: 1,
-    entryType: 'directories'
-};
-// In this example, this variable will store all the paths of the files and directories inside the providen path
-var allFilePaths = [];
-
-readdirp(settings,
-    // This callback is executed everytime a file or directory is found inside the providen path
-    function(fileInfo) {
-        // Store the fullPath of the file/directory in our custom array 
-        allFilePaths.push(
-            fileInfo
-        );
-    }, 
-
-    // This callback is executed once 
-    function (err, res) {
-        if(err){
-            throw err;
+    logger.info('Removing snapshots older than ' + constants.DELETE_OLDER_THAN_SECONDS + ' seconds');
+    findRemoveSync(constants.XPX_SNAPSHOT_SOURCE + '/' + constants.XPX_NETWORK + '/', {
+        dir: '*',
+        file: '*.*',
+        age: {
+            seconds: constants.DELETE_OLDER_THAN_SECONDS
         }
-        var ctime = 0;
-        var lastSnapshot = null;
-        for (var i = 0; i < allFilePaths.length; i++) {
-            if (allFilePaths[i].stat) {
-                logger.debug('Ctime: ' + allFilePaths[i].stat.ctime);
-                if (allFilePaths[i].stat.ctime >= ctime) {
-                    ctime = allFilePaths[i].stat.ctime;
-                    lastSnapshot = allFilePaths[i];
+    });
+    var deleteCommand = 'find ' + constants.XPX_SNAPSHOT_DESTINATION + ' -name \"*\" -type f -mmin +' + constants.DELETE_OLDER_THAN_SECONDS / 60 + ' -delete';
+    var deleteCommandOptions = {
+        cwd: constants.XPX_SNAPSHOT_DESTINATION,
+        stdio: 'inherit'
+    };
+    execSync(deleteCommand, deleteCommandOptions);
+
+    logger.info('Start snapshot creation');
+    execSync(snapshotCommand, snapshotCommandOptions);
+    logger.info('Snapshot created SUCCESSFULLY');
+    logger.info('Zip snapshot and copy to destination folder');
+
+    var settings = {
+        root: constants.XPX_SNAPSHOT_SOURCE + '/' + constants.XPX_NETWORK + '/',
+        depth: 1,
+        entryType: 'directories'
+    };
+    // In this example, this variable will store all the paths of the files and directories inside the providen path
+    var allFilePaths = [];
+
+    readdirp(settings,
+        // This callback is executed everytime a file or directory is found inside the providen path
+        function (fileInfo) {
+            // Store the fullPath of the file/directory in our custom array 
+            allFilePaths.push(
+                fileInfo
+            );
+        },
+
+        // This callback is executed once 
+        function (err, res) {
+            if (err) {
+                throw err;
+            }
+            var ctime = 0;
+            var lastSnapshot = null;
+            for (var i = 0; i < allFilePaths.length; i++) {
+                if (allFilePaths[i].stat) {
+                    logger.debug('Ctime: ' + allFilePaths[i].stat.ctime);
+                    if (allFilePaths[i].stat.ctime >= ctime) {
+                        ctime = allFilePaths[i].stat.ctime;
+                        lastSnapshot = allFilePaths[i];
+                    }
                 }
             }
+            if (lastSnapshot) {
+                logger.info('Last Snapshot: ' + lastSnapshot.fullPath);
+                var zipCommand = 'tar -zcvf ' + lastSnapshot.name + '.tar.gz ' + lastSnapshot.name;
+                var zipCommandOptions = {
+                    cwd: lastSnapshot.fullParentDir,
+                    stdio: 'inherit'
+                };
+                execSync(zipCommand, zipCommandOptions);
+                var moveCommand = 'mv ' + lastSnapshot.name + '.tar.gz ' + constants.XPX_SNAPSHOT_DESTINATION + '/' + lastSnapshot.name + '.tar.gz';
+                execSync(moveCommand, zipCommandOptions);
+            }
+            logger.info('End');
         }
-        if (lastSnapshot) {
-            logger.info('Last Snapshot: ' + lastSnapshot.fullPath);
-            var zipCommand = 'tar -zcvf ' + lastSnapshot.name + '.tar.gz ' + lastSnapshot.name;
-            var zipCommandOptions = {
-                cwd: lastSnapshot.fullParentDir,
-                stdio: 'inherit'
-            };
-            execSync(zipCommand, zipCommandOptions);
-            var moveCommand = 'mv ' + lastSnapshot.name + '.tar.gz ' + constants.XPX_SNAPSHOT_DESTINATION + '/' + lastSnapshot.name + '.tar.gz';
-            execSync(moveCommand, zipCommandOptions);
-        }
-        logger.info('End');
-    }
-);
+    );
+}
+
+createSnapshot();
+//var createSnapshotThread = setInterval(createSnapshot, constants.EXECUTE_EVERY_SECONDS * 1000);
